2d9c6bd7d48a069e6c8afb16a5b52c62c06df69c
Hagoeun <sera1193@naver.com>
2024-02-01 13:33:08 +0900

    first commit

---
+++ src/main/java/beyondvision/BeyondvisionApplication.java
@@ -0,0 +1,13 @@
+package beyondvision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeyondvisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeyondvisionApplication.class, args);
+	}
+
+}
