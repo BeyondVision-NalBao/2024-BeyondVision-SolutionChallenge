b15dfa719304fb2d94dd6f5992f7d4f325f1eb9a
kyungwon <kyungwon0928@daum.net>
2024-02-02 22:31:50 +0900

    feat: add functionality to setting - 숫자를 입력받아 운동목표 수정 - 회원탈퇴 알림창

--- lib/ui/setting/setting.dart
+++ lib/ui/setting/setting.dart
@@ -1,5 +1,7 @@
 import 'package:beyond_vision/core/constants.dart';
 import 'package:beyond_vision/ui/home/widgets/home_grid_button.dart';
+import 'package:beyond_vision/ui/setting/widgets/edit_goal.dart';
+import 'package:beyond_vision/ui/setting/widgets/setting_box.dart';
 import 'package:flutter/material.dart';
 
 class Setting extends StatelessWidget {
@@ -8,20 +10,25 @@ class Setting extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.black,
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            const Icon(speakerIcon, color: Color(fontYellowColor), size: 50),
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.7,
-              child: GridView.count(
-                crossAxisCount: 2,
-                children: const [HomeButton(name: "앱 설정")],
-              ),
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.black,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          const Icon(speakerIcon, color: Color(fontYellowColor), size: 50),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.7,
+            child: GridView.count(
+              crossAxisCount: 2,
+              children: const [
+                SettingBox(name: "운동 목표\n수정"),
+                SettingBox(name: "회원탈퇴"),
+              ],
             ),
-          ],
-        ));
+          ),
+        ],
+      ),
+    );
   }
 }
---
+++ lib/ui/setting/widgets/setting_box.dart
@@ -0,0 +1,58 @@
+import 'package:beyond_vision/core/constants.dart';
+import 'package:beyond_vision/ui/record/record.dart';
+import 'package:beyond_vision/ui/routine/routine.dart';
+import 'package:beyond_vision/ui/setting/setting.dart';
+import 'package:beyond_vision/ui/setting/widgets/delete_account.dart';
+import 'package:beyond_vision/ui/setting/widgets/edit_goal.dart';
+import 'package:flutter/material.dart';
+
+class SettingBox extends StatelessWidget {
+  final String name;
+  const SettingBox({super.key, required this.name});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(10),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          padding: const EdgeInsets.all(10),
+          backgroundColor: const Color(boxColor),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30),
+          ),
+        ),
+        onPressed: () {
+          if (name == "운동 목표\n수정") {
+            showDialog(
+                context: context,
+                builder: (BuildContext context) => const EditGoal());
+          } else if (name == "회원탈퇴") {
+            showDialog(
+                context: context,
+                builder: (BuildContext context) => const DeleteAccount());
+          } else if (name == "운동 기록") {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => const Record()));
+          } else {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => const Setting()));
+          }
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Center(
+            child: Text(
+              name,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                  fontSize: 36,
+                  color: Color(fontYellowColor),
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
