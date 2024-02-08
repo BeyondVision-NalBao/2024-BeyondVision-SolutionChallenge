a600faf7b3c6fe876ffb7476a634f841110c6380
kyungwon <kyungwon0928@daum.net>
2024-02-08 18:59:40 +0900

    refactor: create login_service - 로그인 시 필요한 함수들을 login Service에서 제공하도록 변경

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:beyond_vision/provider/date_provider.dart';
 import 'package:beyond_vision/service/date_service.dart';
+import 'package:beyond_vision/service/login_service.dart';
 import 'package:beyond_vision/ui/home/home.dart';
 import 'package:beyond_vision/ui/login/google_login.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:provider/provider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   initializeDateFormatting().then((_) => runApp(ChangeNotifierProvider(
@@ -20,25 +20,13 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  final routes = {HomePage: (context) => const HomePage()};
-  DateService dateService = DateService();
-
+  LoginService loginService = LoginService();
   bool isLogined = false;
 
-  Future<void> checkLogin() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    if (prefs.getString('loginDate') != null &&
-        prefs.getBool('isLogined') != null) {
-      if (dateService.compareDate(prefs.getString('loginDate')!)) {
-        isLogined = prefs.getBool('isLogined')!;
-      }
-    }
-  }
-
   @override
-  void initState() {
+  void initState() async {
     // TODO: implement initState
-    checkLogin();
+    isLogined = await loginService.checkLogin();
     super.initState();
   }
 
