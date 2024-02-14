c7d1f6166d97ea857922ea2b204f274779cb8db5
Hagoeun <sera1193@naver.com>
2024-02-14 18:49:03 +0900

    feat: input 파일 하나로 합치기

--- 2차파일/api_Flask/frame.py
+++ 2차파일/api_Flask/frame.py
@@ -14,9 +14,9 @@ def start(memberId, routineId, routineDetailId):
     conn = connect_to_db()
     cursor = conn.cursor()
     cursor.execute("""SELECT rd.exercise_name, rd.exercise_count 
-                   FROM routine r 
-                   JOIN routine_detail rd ON r.id = rd.routine_id 
-                   WHERE r.member_id = %s AND r.id = %s AND rd.id = %s;""", (memberId, routineId, routineDetailId))
+                    FROM routine r 
+                    JOIN routine_detail rd ON r.id = rd.routine_id 
+                    WHERE r.member_id = %s AND r.id = %s AND rd.id = %s;""", (memberId, routineId, routineDetailId))
     results = cursor.fetchall() #여기에 운동정보, 횟수, 고객정보 들어있음
     cursor.close()
     conn.close()
@@ -31,10 +31,44 @@ if not os.path.exists(UPLOAD_FOLDER):
 
 @app.route("/frame", methods=["POST"])
 def frame():
-     file = request.files['frame']
-     camera.gen(file)
-     return "Success"
+    file = request.files['frame']
+    camera.gen(file)
+    return "Success"
 
+@app.route("/api/v1/ml/<int:memberId>/ready", methods=["POST"])
+def ready(memberId):
+    data = request.json
+    routineName = data['routineName']
+
+    conn = connect_to_db()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM routine_detail WHERE routine_id = (SELECT id FROM routine WHERE member_id = %s AND name = %s)", (memberId, routineName))
+    results = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    
+    if results:
+        formatted_results = []
+        for row in results:
+            formatted_row = {}
+            formatted_row['exercise_count'] = row[0]
+            formatted_row['exercise_order'] = row[1]
+            formatted_row['exercise_id'] = row[2]
+            formatted_row['id'] = row[3]
+            formatted_row['routineId'] = row[4]
+            formatted_row['exercise_name'] = row[5]
+            formatted_results.append(formatted_row)
+        return Response(str(formatted_results), status=200, mimetype='application/json')
+    else:
+        return Response("No routine found for given memberId and routineName", status=404, mimetype='application/json')
+
+#@app.route("/<long:memberId>/<long:routineDetailId>/frame", method=['POST'])
+#def frame(memberId, routineDetailId):
+#  output.save(camera.gen(images))
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
