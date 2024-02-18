78817d7ac4900ce26f154cf2f56baf65d422651f
youngseo cho <dudtj0412@naver.com>
2024-02-18 15:01:49 +0900

    Fix: 운동시작 API 스펙 변경

--- 2차파일/api_Flask/frame.py
+++ 2차파일/api_Flask/frame.py
@@ -9,34 +9,9 @@ import camera
 
 app = Flask(__name__)
 
-@app.route("/<int:memberId>/<int:routineId>/<int:routineDetailId>/start", methods=["POST"]) #클라이언트 정보를 select운동 어쩌구에 넘기는 함수
-def start(memberId, routineId, routineDetailId):
-    conn = connect_to_db()
-    cursor = conn.cursor()
-    cursor.execute("""SELECT rd.exercise_name, rd.exercise_count 
-                    FROM routine r 
-                    JOIN routine_detail rd ON r.id = rd.routine_id 
-                    WHERE r.member_id = %s AND r.id = %s AND rd.id = %s;""", (memberId, routineId, routineDetailId))
-    results = cursor.fetchall() #여기에 운동정보, 횟수, 고객정보 들어있음
-    cursor.close()
-    conn.close()
-    result = ready.selectExercise(results[0][0],results[0][1])
-    #print(result)
-    return str(result) #postnat
-
-
-UPLOAD_FOLDER = 'uploads'
-if not os.path.exists(UPLOAD_FOLDER):
-    os.makedirs(UPLOAD_FOLDER)
-
-@app.route("/frame", methods=["POST"])
-def frame():
-    file = request.files['frame']
-    camera.gen(file)
-    return "Success"
-
+#미사용
 @app.route("/api/v1/ml/<int:memberId>/ready", methods=["POST"])
-def ready(memberId):
+def ready_exercise(memberId):
     data = request.json
     routineName = data['routineName']
 
@@ -62,9 +37,41 @@ def ready(memberId):
     else:
         return Response("No routine found for given memberId and routineName", status=404, mimetype='application/json')
 
-#@app.route("/<long:memberId>/<long:routineDetailId>/frame", method=['POST'])
-#def frame(memberId, routineDetailId):
-#  output.save(camera.gen(images))
+#API 스펙 수정
+@app.route("/<int:memberId>/start", methods=["POST"]) #클라이언트 정보를 select운동 어쩌구에 넘기는 함수
+def start_exercise(memberId):
+    data = request.json
+    routineName = data['routineName']
+    routineCount = data['routineCount']
+
+    #미사용 부분   
+    #conn = connect_to_db()
+    #cursor = conn.cursor()
+    #cursor.execute("""SELECT rd.exercise_name, rd.exercise_count 
+    #               FROM routine r 
+    #               JOIN routine_detail rd ON r.id = rd.routine_id 
+    #              WHERE r.member_id = %s AND r.id = %s AND rd.id = %s;""", (memberId, routineId, routineDetailId))
+    #results = cursor.fetchall() #여기에 운동정보, 횟수, 고객정보 들어있음
+    #cursor.close()
+    #conn.close()
+
+    result = ready.selectExercise(routineName,routineCount)
+    print(result)
+    return str(result) #postnat
+
+
+UPLOAD_FOLDER = 'uploads'
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+@app.route("/frame", methods=["POST"])
+def frame():
+    file = request.files['frame']
+    camera.gen(file)
+    return "Success"
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
