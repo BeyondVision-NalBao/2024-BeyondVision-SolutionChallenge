310347dad5b59b391fd6539a27d937f386102913
kyungwon <kyungwon0928@daum.net>
2024-02-19 18:58:52 +0900

    feat: camera 서버 문제 모두 해결

--- lib/ui/workout/widgets/workout_camera_view.dart
+++ lib/ui/workout/widgets/workout_camera_view.dart
@@ -8,6 +8,11 @@ import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
 import 'package:http/http.dart' as http;
 import 'package:provider/provider.dart';
+import 'dart:async';
+import 'package:dio/dio.dart';
+import 'dart:typed_data';
+
+import 'package:wakelock/wakelock.dart';
 
 List<CameraDescription> cameras = [];
 
@@ -24,10 +29,12 @@ class _CameraViewState extends State<CameraView> {
   late CameraController _controller;
   late bool _isInitialized;
   bool _isStreamingPaused = false;
+  late Timer _timer;
 
   @override
   void initState() {
     super.initState();
+    Wakelock.enable();
     _isInitialized = false;
     _initializeCamera();
   }
@@ -49,15 +56,18 @@ class _CameraViewState extends State<CameraView> {
 
     bool isReady =
         await getReady(auth.memberId, widget.workout.name, widget.count ?? 30);
-
-    if (!_controller.value.isStreamingImages && isReady) {
-      await _controller.startImageStream((CameraImage image) async {
-        if (!_isStreamingPaused) {
-          List<int> bytes = image.planes[0].bytes;
-          _sendFrameToServer(bytes);
-        }
-      });
-    }
+    _timer = Timer.periodic(const Duration(seconds: 2), (_) {
+      if (!_isStreamingPaused) {
+        _controller.takePicture().then((XFile? file) {
+          if (file != null) {
+            // 이미지 파일을 읽고 서버에 보내는 로직
+            File imageFile = File(file.path);
+            List<int> imageBytes = imageFile.readAsBytesSync();
+            _sendFrameToServer(imageBytes);
+          }
+        });
+      }
+    });
   }
 
   Future<bool> getReady(
@@ -77,15 +87,31 @@ class _CameraViewState extends State<CameraView> {
   }
 
   void _sendFrameToServer(List<int> bytes) async {
-    String url = 'http://your-flask-server-url:5000/process_frame';
+    String url = 'http://34.64.89.205:5000/frame';
     try {
       // Send POST request with image bytes
-      http.Response response = await http.post(
-        Uri.parse(url),
-        body: jsonEncode({'image': base64Encode(bytes)}),
-        headers: {'Content-Type': 'application/json'},
-      );
+      // 바이트 리스트를 Uint8List로 변환
+      Uint8List uint8list = Uint8List.fromList(bytes);
 
+      // Uint8List를 FormData에 추가
+      FormData formData = FormData.fromMap({
+        'frame': MultipartFile.fromBytes(
+          uint8list,
+          filename: 'image.png',
+        ),
+      });
+      // Dio 인스턴스 생성
+      Dio dio = Dio();
+
+      // POST 요청 보내기
+      Response response = await dio.post(
+        url,
+        data: formData,
+        options: Options(
+          contentType: 'multipart/form-data',
+        ),
+      );
+      print(response.statusCode);
       if (response.statusCode == 200) {
         // Handle response from server if needed
         print('Frame processed successfully');
@@ -99,7 +125,9 @@ class _CameraViewState extends State<CameraView> {
 
   @override
   void dispose() {
+    _timer.cancel(); // 타이머 해제
     _controller.dispose();
+    Wakelock.disable();
     super.dispose();
   }
 
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,6 +11,7 @@ import google_sign_in_ios
 import path_provider_foundation
 import shared_preferences_foundation
 import speech_to_text_macos
+import wakelock_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterImageCompressMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterImageCompressMacosPlugin"))
@@ -19,4 +20,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SpeechToTextMacosPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextMacosPlugin"))
+  WakelockMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockMacosPlugin"))
 }
