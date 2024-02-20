59c0ef583e9a218910a7d6844aa10b0bf83e652c
youngseo cho <dudtj0412@naver.com>
2024-02-20 16:45:29 +0900

    Feat: 운동 랜덤 조회 API

--- src/main/java/beyondvision/exercise/domain/repository/ExerciseRepository.java
+++ src/main/java/beyondvision/exercise/domain/repository/ExerciseRepository.java
@@ -3,13 +3,16 @@ package beyondvision.exercise.domain.repository;
 import beyondvision.exercise.domain.Exercise;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.Random;
 
 public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
 
-    Optional<Exercise> findExerciseById(final Long exerciseId);
+    Exercise findExerciseById(final Long exerciseId);
 
     Exercise findByName(final String exerciseName);
     List<Exercise> findByCategoryNumber(final Integer CategoryNumber);
+
 }
