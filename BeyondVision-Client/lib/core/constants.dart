4636578b8a51183f30793844c7a79e4f4c8149ac
Kyungwon Shin <93874872+kyungwonS928@users.noreply.github.com>
2024-02-26 01:31:33 +0900

    Delete BeyondVision-Client directory

--- BeyondVision-Client/lib/core/constants.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-//Color
-const int boxColor = 0xff2F2E2C;
-const int fontYellowColor = 0xffFFD400;
-const int blurYellowColor = 0xff5C5948;
-
-//Icon
-const IconData speakerIcon = Icons.volume_up;
